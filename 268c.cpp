75564653d3410d5444e499f2dca2b3806c07e6a6
vkayy <vkrice2475@gmail.com>
2024-04-10 08:58:14 +0100

    feat: add 268c

---
+++ 268c.cpp
@@ -0,0 +1,41 @@
+// g++ $file.cpp -std=c++17 -o $file && time ./$file < $file-in.txt
+
+#include "bits/stdc++.h"
+// #include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vii;
+#define FASTIO                \
+    ios ::sync_with_stdio(0); \
+    cin.tie(0);               \
+    cout.tie(0);
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define nl "\n"
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define IN(i,l,r) (l<i&&i<r)
+#define LINR(i,l,r) (l<=i&&i<=r)
+#define LIN(i,l,r) (l<=i&&i<r)
+#define INR(i,l,r) (l<i&&i<=r)
+
+void solve() {
+    ll n, m;
+    cin>>n>>m;
+    ll k = min(n, m);
+    cout<<k+1<<nl;
+    rep(i, 0, k+1) {
+        cout<<i<<" "<<k-i<<nl;
+    }
+}
+
+int main() {
+    FASTIO
+    solve();
+    return 0;
+}
+
